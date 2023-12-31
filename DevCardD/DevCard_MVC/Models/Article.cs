85b7e6154d4835736acb16b3d2337e21be31d778
MobinGholizadeh <mobingithub@gmail.com>
2023-12-31 12:22:29 +0330

    Prjects section from index page moved to partial view Projects

---
+++ DevCardD/DevCard_MVC/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace DevCard_MVC.Models
+{
+	public class Article
+	{
+		public long Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+
+	}
+}
---
+++ DevCardD/DevCard_MVC/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace DevCard_MVC.Models
+{
+	public class Project
+	{
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+
+}
