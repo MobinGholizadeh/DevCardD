11c2f00c15cc28efdf796bf6db0fa67dbd8e040c
MobinGholizadeh <mobingithub@gmail.com>
2023-12-31 11:33:20 +0330

    Index & contact page added

--- DevCardD/DevCard_MVC/Controllers/HomeController.cs
+++ DevCardD/DevCard_MVC/Controllers/HomeController.cs
@@ -6,22 +6,24 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController()
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        
+        public IActionResult Contact()
         {
             return View();
         }
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
