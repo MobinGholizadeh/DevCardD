f5df10d1f105efb3805c155d2b651cd359dfaa05
MobinGholizadeh <mobingithub@gmail.com>
2024-01-02 09:46:34 +0330

    forms server side validation has been done

--- DevCardD/DevCard_MVC/Controllers/HomeController.cs
+++ DevCardD/DevCard_MVC/Controllers/HomeController.cs
@@ -5,48 +5,55 @@ using System.Diagnostics;
 
 namespace DevCard_MVC.Controllers
 {
-    public class HomeController : Controller
-    {
+	public class HomeController : Controller
+	{
 
 
-        public HomeController()
-        {
+		public HomeController()
+		{
 
-        }
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpGet]
-        public IActionResult Contact()
-        {
-            var model = new ContactForm();
-            return View(model);
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpGet]
+		public IActionResult Contact()
+		{
+			var model = new ContactForm();
+			return View(model);
+		}
 
-        //[HttpPost]
-        //public JsonResult Contact(IFormCollection form)
-        //{
-        //    var name = form["name"];
-        //    return Json(Ok());
-        //}
-
-
-        [HttpPost]
-        public JsonResult Contact(ContactForm form)
-
-        {
-            Console.WriteLine(form.ToString());
-            return Json(Ok());
-            
-        }
+		//[HttpPost]
+		//public JsonResult Contact(IFormCollection form)
+		//{
+		//    var name = form["name"];
+		//    return Json(Ok());
+		//}
 
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[HttpPost]
+		public IActionResult Contact(ContactForm model)
+
+		{
+
+			if (!ModelState.IsValid)
+			{
+				ViewBag.error = "Information that has been entered is wrong , try again please ";
+				return View(model);
+			}
+
+			//return RedirectToAction("Index");
+			ViewBag.success = "Your message has been sent successfully! thank you <3 ";
+			return View();
+		}
+
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
